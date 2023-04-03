c00fc2f5f621da25c6688a795e4800c30b799380
Asia <asiaverner666@gmail.com>
2023-04-03 23:01:21 +0300

    Задание 1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите первое число: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        double b = double.Parse(Console.ReadLine());
+
+        if (a > b)
+        {
+            Console.WriteLine("Максимальное число: " + a);
+            Console.WriteLine("Минимальное число: " + b);
+        }
+        else if (b > a)
+        {
+            Console.WriteLine("Максимальное число: " + b);
+            Console.WriteLine("Минимальное число: " + a);
+        }
+        else
+        {
+            Console.WriteLine("Числа равны");
+        }
+    }
+}
