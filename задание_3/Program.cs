a7dbb95658d568251099519b57a083a5a8355a02
Asia <asiaverner666@gmail.com>
2023-04-10 13:18:13 +0300

    3 задание

---
+++ задание_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Введите число:");
+        int num = int.Parse(Console.ReadLine());
+        
+        if (num % 2 == 0) {
+            Console.WriteLine("Да, число чётное.");
+        } else {
+            Console.WriteLine("Нет, число нечётное.");
+        }
+    }
+}
